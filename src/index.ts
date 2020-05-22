e80c7c8bdd81d4cc8dce92f5c1f54df979a3a772
emilychiu <you@example.com>
2020-05-21 18:17:45 -0700

    WE FIXED DA BUG

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 import { move } from './logic'
 
 const userProgress = window.location.search.replace('?', '')
-//this is where the boilerplate goes I think
-if(userProgress === '') {
-   move(0)
+if (userProgress === '') {
+    move(0)
+} else {
+
+    const stateIds = userProgress.split(';')
+
+
+    console.log(userProgress)
+
+    const callbackMove = (id) => {
+        const numId = parseInt(id)
+        move(numId)
+    }
+    stateIds.forEach(callbackMove)
 }
-const stateIds = userProgress.split(';')
-
-
-console.log(userProgress)
-
-const callbackMove = (id) => {
-    const numId = parseInt(id)
-    move(numId)    
-}
-stateIds.forEach(callbackMove)
